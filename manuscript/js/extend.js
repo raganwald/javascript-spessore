af32e22c48a8c4a17702cc88acc48b0feb78b9ed
Reginald Braithwaite <reg.braithwaite@github.com>
2014-04-28 19:33:52 -0400

    big idea update

--- manuscript/js/extend.js
+++ manuscript/js/extend.js
@@ -5,15 +5,12 @@ function extend () {
       providers = __slice.call(arguments, 1),
       key,
       i,
-      provider,
-      except;
+      provider;
 
   for (i = 0; i < providers.length; ++i) {
     provider = providers[i];
-    except = provider['except'] || [];
-    except.push('except');
     for (key in provider) {
-      if (except.indexOf(key) < 0 && provider.hasOwnProperty(key)) {
+      if (provider.hasOwnProperty(key)) {
         consumer[key] = provider[key];
       };
     };
